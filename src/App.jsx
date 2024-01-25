10cebf0d95b0cb892829fb8138d93539fcc23232
Lemito66 <lemito66@gmail.com>
2024-01-25 14:30:24 -0500

    creación de modo oscuro para react

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const [theme, setTheme] = useState(() => {
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      return "dark";
+    }
+    return "light";
+  });
+
+  useEffect(() => {
+    if (theme === "light") {
+      document.querySelector("html").classList.remove("dark");
+    } else {
+      document.querySelector("html").classList.add("dark");
+    }
+  }, [theme]);
+
+  const handleChangeTheme = () => {
+    setTheme((preTheme) => (preTheme === "light" ? "dark" : "light"));
+    setCount((preCount) => preCount + 1);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="h-screen flex justify-center items-center dark:bg-neutral-900">
+      <button
+        className="bg-slate-200 px-4 py-2 rounded-lg hover:bg-slate-300 dark:bg-slate-700 dark:hover:bg-slate-600 dark:text-white"
+        onClick={handleChangeTheme}
+      >
+        Change Theme {count}
+      </button>
+    </div>
+  );
 }
 
-export default App
+export default App;
